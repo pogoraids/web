5582bca42f6a80f4afdc9a5aa45344cb3ebad80f
Mathías Rodríguez <mathias.rodriguez@gmail.com>
2019-05-22 15:21:10 -0300

    fix: guard names && imports

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements Resolve<any> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+    return true;
+  }
+}
